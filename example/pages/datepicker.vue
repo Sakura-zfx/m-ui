8ab8a1d9a2d19efc514e3ccccd4c7701e2e1c5eb
yangming <472102644@qq.com>
2018-06-04 10:59:07 +0800

    [fix] datepicker doc

--- example/pages/datepicker.vue
+++ example/pages/datepicker.vue
@@ -6,7 +6,7 @@
       :sub-color="`#ebe2ed`"
       :is-single="false"
       :default-time-stamp="new Date('2018-05-14 14:00').getTime()"
-      :time-label="['label1', 'label2']"
+      :time-label="['label1', 'label23']"
       @date-select="handleDateSelect"
       @select-finish="handleDateChoose"
     />
@@ -26,33 +26,33 @@
       <p>props</p>
       <pre class="px-font-12">
        props: {
-         limitDays: {
-           type: Number,
-           default: 90
-         },
-         mainColor: {
-           type: String,
-           default: '#753e81'
-         },
-         subColor: {
-           type: String,
-           default: '#ebe2ed'
-         },
-         oneWay: {
-           type: Boolean,
-           default: false
-         },
-         timeStamp: {
-           type: Number | String,
-           default: Date.now()
-         },
-         textArr: {
-           type: Array,
-           default() {
-             return ['入店', '离店']
-           }
-         }
-      }
+          limitDays: {
+            type: Number,
+            default: 90
+          },
+          mainColor: {
+            type: String,
+            default: '#753e81'
+          },
+          subColor: {
+            type: String,
+            default: '#ebe2ed'
+          },
+          isSingle: {
+            type: Boolean,
+            default: false
+          },
+          defaultTimeStamp: {
+            type: Number | String,
+            default: Date.now()
+          },
+          timeLabel: {
+            type: Array,
+            default() {
+              return ['入店', '离店']
+            }
+          }
+       }
       </pre>
     </common-entry>
   </div>
