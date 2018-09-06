74e4af711a15f239e877959404860248b0c5f710
yangming <472102644@qq.com>
2018-09-06 10:25:31 +0800

    [fix] overflow

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ import Toast from './components/toast'
 import TravelPanel from './components/travel-panel'
 import UserPanel from './components/user-panel'
 
-const version = '1.0.102'
+const version = '1.0.103'
 const components = [
   AddressSelect,
   ApproveSelect,
