a3c6c4434407e9cf299b18ed183b3d5b24c37002
yangming <472102644@qq.com>
2018-11-07 10:11:03 +0800

    fix: bottom-tab add disabled

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ import Toast from './components/toast'
 import TravelPanel from './components/travel-panel'
 import UserPanel from './components/user-panel'
 
-const version = '1.0.125'
+const version = '1.0.126'
 const components = [
   AddressSelect,
   ApproveSelect,
