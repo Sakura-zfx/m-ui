5948064c8fdb1234a1882c32c8868dfe062d66bd
yangming <472102644@qq.com>
2019-05-06 18:36:07 +0800

    build: (master)打包

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ import UserPanel from './components/user-panel'
 import WelfareInput from './components/welfare-input'
 import WordLine from './components/word-line'
 
-const version = '2.0.46'
+const version = '2.0.47'
 const components = [
   AddressSelect,
   ApproveSelect,
